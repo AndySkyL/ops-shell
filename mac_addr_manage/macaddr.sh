eceb5db84817b4d77b7b26f83ef1bb41da7b53ad
andy_tang <962824378@qq.com>
2016-12-23 12:59:25 +0800

    update macaddr.sh

--- mac_addr_manage/macaddr.sh
+++ mac_addr_manage/macaddr.sh
@@ -7,6 +7,8 @@ DEVICE_TYPE="$3"
 MAC_ADDR="$4"
 array=()
 LOCK_FILE="/tmp/macaddr.lock"
+MYSQL_PASS="654321"
+SQL_EXE="mysql -uroot -p$MYSQL_PASS -e"
 
 usage(){
   echo "USAGE: $0 { add|del|list } USERNAME { pc|moblie|pad_kindle|other } MAC_ADDR"
@@ -14,9 +16,9 @@ usage(){
 }
 
 add() {
-CNUM=`mysql -uroot -p654321 -e "use mac_list; select * from maclist where name='$USER_NAME';"|wc -l `
+CNUM=`$SQL_EXE "use mac_list; select * from maclist where name='$USER_NAME';"|wc -l `
 	if [ $CNUM -eq 2  ];then
-    MAC_CONT=`mysql -uroot -p654321 -e "use mac_list; select * from maclist where name='$USER_NAME';"|sed -n 2p`
+    MAC_CONT=`$SQL_EXE "use mac_list; select * from maclist where name='$USER_NAME';"|sed -n 2p`
 		for n in {2..11}
           do
             array[$n]=`echo ${MAC_CONT}|awk '{print $'$n'}'`
@@ -30,7 +32,7 @@ case $DEVICE_TYPE in
    for i in {3..4} ;do
 	j=$[j+1] 
 #	  echo ${array[$i]}	
-	   [ -z ${array[$i]} ]||[ ${array[$i]} == "NULL" ]&& `mysql -uroot -p654321 -e "use mac_list; update maclist set computer$j='$MAC_ADDR' where name='$USER_NAME';"`&&exit
+	   [ -z ${array[$i]} ]||[ ${array[$i]} == "NULL" ]&& `$SQL_EXE "use mac_list; update maclist set computer$j='$MAC_ADDR' where name='$USER_NAME';"`&&exit
    done
 	   error;
    ;;   
@@ -41,19 +43,19 @@ case $DEVICE_TYPE in
 	   x=$[x+1]
 #		   echo $x
 #		   echo ${array[$m]}
-      [ -z ${array[${m}]} ]||[ ${array[${m}]} == "NULL" ] && `mysql -uroot -p654321 -e "use mac_list; update maclist set mobile$x='$MAC_ADDR' where name='$USER_NAME';"`&&exit 
+      [ -z ${array[${m}]} ]||[ ${array[${m}]} == "NULL" ] && `$SQL_EXE "use mac_list; update maclist set mobile$x='$MAC_ADDR' where name='$USER_NAME';"`&&exit 
 	done
 	  error;
    ;;
 
    kindle_pad)
-	 [ -z ${array[10]} ]||[ ${array[10]} == "NULL" ] && `mysql -uroot -p654321 -e "use mac_list; update maclist set kindle_pad='$MAC_ADDR' where name='$USER_NAME';"`&&exit
+	 [ -z ${array[10]} ]||[ ${array[10]} == "NULL" ] && `$SQL_EXE "use mac_list; update maclist set kindle_pad='$MAC_ADDR' where name='$USER_NAME';"`&&exit
 	 error;
 
 	 ;;
 
    other)
-	   [ -z ${array[10]} ]||[ ${array[10]} -eq "NULL" ] &&`mysql -uroot -p654321 -e "use mac_list; update maclist set vm_other='$MAC_ADDR' where name='$USER_NAME';"`&&exit
+	   [ -z ${array[10]} ]||[ ${array[10]} -eq "NULL" ] &&`$SQL_EXE "use mac_list; update maclist set vm_other='$MAC_ADDR' where name='$USER_NAME';"`&&exit
 	   error;
 
 	;;
@@ -67,21 +69,21 @@ case $DEVICE_TYPE in
 
 		   case  $DEVICE_TYPE in
 			   pc)
-		  `mysql -uroot -p654321 -e "use mac_list;insert maclist (name,computer1) values ('$USER_NAME','$MAC_ADDR');"`
+		  `$SQL_EXE "use mac_list;insert maclist (name,computer1) values ('$USER_NAME','$MAC_ADDR');"`
 			   ;;
 
 	           mobile)
 
-		  `mysql -uroot -p654321 -e "use mac_list; insert maclist (name,mobile1) values ('$USER_NAME','$MAC_ADDR');"`
+		  `$SQL_EXE "use mac_list; insert maclist (name,mobile1) values ('$USER_NAME','$MAC_ADDR');"`
 
 			  ;;
 
 			   kindle_pad)
-		  `mysql -uroot -p654321 -e "use mac_list; insert maclist (name,kindle_pad) values ('$USER_NAME','$MAC_ADDR');"`
+		  `$SQL_EXE "use mac_list; insert maclist (name,kindle_pad) values ('$USER_NAME','$MAC_ADDR');"`
 		      ;;
                other)
 
-		  `mysql -uroot -p654321 -e "use mac_list; insert maclist (name,vm_other) values ('$USER_NAME','$MAC_ADDR');"`
+		  `$SQL_EXE "use mac_list; insert maclist (name,vm_other) values ('$USER_NAME','$MAC_ADDR');"`
               ;;
 
 			   *)
@@ -95,7 +97,7 @@ fi
 
 del() {
 
-CNUM=`mysql -uroot -p654321 -e "use mac_list; select * from maclist where name='$USER_NAME';"|wc -l `
+CNUM=`$SQL_EXE "use mac_list; select * from maclist where name='$USER_NAME';"|wc -l `
 if [ $CNUM -eq 0 ];then
   echo "the user not exist!" 
   exit 2;
@@ -103,25 +105,25 @@ if [ $CNUM -eq 0 ];then
 else 
 	case $DEVICE_TYPE in
 		all)
-		`mysql -uroot -p654321 -e "use mac_list; delete from maclist where name='$USER_NAME';"`
+		`$SQL_EXE "use mac_list; delete from maclist where name='$USER_NAME';"`
 		;;
 
 		computer*)
-		`mysql -uroot -p654321 -e "use mac_list; update maclist set $DEVICE_TYPE=NULL where name='$USER_NAME';"`
+		`$SQL_EXE "use mac_list; update maclist set $DEVICE_TYPE=NULL where name='$USER_NAME';"`
 	    ;;
 
 	    mobile*)
-		`mysql -uroot -p654321 -e "use mac_list; update maclist set $DEVICE_TYPE=NULL where name='$USER_NAME';"`
+		`$SQL_EXE "use mac_list; update maclist set $DEVICE_TYPE=NULL where name='$USER_NAME';"`
 	    ;;
 
 
 	    kindle_pad)
-		`mysql -uroot -p654321 -e "use mac_list; update maclist set $DEVICE_TYPE=NULL where name='$USER_NAME';"`
+		`$SQL_EXE "use mac_list; update maclist set $DEVICE_TYPE=NULL where name='$USER_NAME';"`
 	    ;;
 
 
 	    vm_other)
-		`mysql -uroot -p654321 -e "use mac_list; update maclist set $DEVICE_TYPE=NULL where name='$USER_NAME';"`
+		`$SQL_EXE "use mac_list; update maclist set $DEVICE_TYPE=NULL where name='$USER_NAME';"`
 	    ;;
 
 		*)
@@ -156,7 +158,7 @@ main(){
 
         list)
 		[ -z $USER_NAME ]&&usage &&exit	
-    	mysql -uroot -p654321 -e "use mac_list; select * from maclist where name='$USER_NAME';"
+    	$SQL_EXE "use mac_list; select * from maclist where name='$USER_NAME';"
 		;;
 
       *)
