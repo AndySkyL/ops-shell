2f221dc47f8aefd07b7456bfa99a244cd0551171
唐新舒 <tangxs@fengkonglm.com>
2018-08-23 10:40:49 +0800

    add find file script

---
+++ tools-scripts/find_archive_data_by_month.sh
@@ -0,0 +1,36 @@
+#/bin/bash
+# Warning: use tar unpress the packages need add "-P" option, eg: tar xfP  [Filename]
+# Recommend use  relative path instead of absolute path when use tar command.
+
+DirPath=/root
+StartTime='2017-04-01 00:00:00'
+EndTime='2017-04-30 23:59:59'
+ArchiveDir=/tmp/test
+ArchiveFile=/tmp/test.tar.gz
+
+[ ! -d $ArchiveDir ] && mkdir -p $ArchiveDir
+
+# cp file to the des dir
+FileFilter(){
+for filename in `find $DirPath/*  -newermt "$StartTime" -a ! -newermt  "$EndTime"`
+  do 
+     cp -r -p $filename  $ArchiveDir/
+done
+
+}
+
+FileArchive(){
+tar zcvPf ${ArchiveFile} ${ArchiveDir} 
+rm -fr $ArchiveDir
+
+}
+main(){
+FileFilter
+FileArchive
+}
+
+main
+
+
+
+
